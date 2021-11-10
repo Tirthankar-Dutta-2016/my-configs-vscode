1a3c880cbb93c5146b3995dfc7bb5f4b95ccf5e9
Tirthankar Dutta <tirthankar.graviton@gmail.com>
2021-11-10 18:52:58 +0530

    changed variable names, added comments

--- install_extns.sh
+++ install_extns.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 
-extn_file="/home/tirthankar/Desktop/extensions.txt"
+# ***********************************************************
+# Replace <file_name>.txt with the actual name of the file 
+# containing all the VS Code extensions
+# ***********************************************************
+file_of_extns="$USER/path/to/<file_name>.txt"
 
-echo $extn_file
+# ***********************************************************
+# Check if the file is read correctly
+# ***********************************************************
+echo 'file name: ' $file_of_extns
 
-for extn in `cat $extn_file`
+
+# ***********************************************************
+# Install the extensions 
+# ***********************************************************
+for extn in `cat $file_of_extns`
 do
    echo $extn
   `code --install-extension --force $extn`
